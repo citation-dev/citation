b93dec603168e1c641832b6ab9f663bdd74ff592
m2en <me@m2en.dev>
2022-09-01 16:51:27 +0900

    Release v1.1.1 (#28)
    * fix: bypass not quote author check
    
    * feat: status log
    
    * docs: remove warn note

--- src/main/kotlin/dev/m2en/citation/QuoteDelete.kt
+++ src/main/kotlin/dev/m2en/citation/QuoteDelete.kt
@@ -5,17 +5,24 @@ import dev.kord.core.event.message.ReactionAddEvent
 import dev.kord.core.kordLogger
 
 suspend fun ReactionAddEvent.onQuoteDelete(selfId: Snowflake) {
-    val message = message.fetchMessage()
+    val targetMessage = message.fetchMessage() // 削除対象のメッセージ
+    val targetMessageReference = targetMessage.messageReference ?: return
 
     if(user.id == selfId) {
         return
     }
 
-    if(message.author?.id != selfId && emoji.name != ":wastebasket:") {
+    if(targetMessage.author?.id != selfId && emoji.name != ":wastebasket:") {
+        return
+    }
+
+    val targetMessageReply = targetMessageReference.message?.id?.let { channel.getMessageOrNull(it) } ?: return
+    if(user.id != targetMessageReply.author?.id) {
+        kordLogger.warn("警告: ${user.asUser().tag} は引用者(${targetMessageReply.author?.tag})ではないため、削除リクエストを却下しました")
         return
     }
 
     // メッセージ送信者自身のメッセージを本人が削除してもDiscordの仕様上監査ログには記録されないため、理由は指定しない
-    message.delete()
-    kordLogger.info("削除リクエストにより、citationのメッセージを削除しました。")
+    targetMessage.delete()
+    kordLogger.info("削除: ${user.asUser().tag} の削除リクエストを受理しました")
 }
