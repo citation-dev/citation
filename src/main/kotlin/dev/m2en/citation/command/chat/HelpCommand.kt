660554fc9e2134b638f7fb8da31023b5bd5d8804
m2en <me@m2en.dev>
2022-09-04 23:32:21 +0900

    refactor: 埋め込みの作成手段を改善

--- src/main/kotlin/dev/m2en/citation/command/chat/HelpCommand.kt
+++ src/main/kotlin/dev/m2en/citation/command/chat/HelpCommand.kt
@@ -5,33 +5,38 @@ import dev.kord.core.behavior.interaction.response.respond
 import dev.kord.core.entity.interaction.GuildChatInputCommandInteraction
 import dev.kord.rest.builder.message.EmbedBuilder
 import dev.m2en.citation.command.InteractionCommandInterface
+import dev.m2en.citation.getCitationVersion
 
 object HelpCommand: InteractionCommandInterface {
 
     override suspend fun onCommand(interaction: GuildChatInputCommandInteraction, responseBehavior: DeferredEphemeralMessageInteractionResponseBehavior) {
-        val embed = EmbedBuilder()
-        embed.title = "ヘルプ"
-        embed.url = "https://github.com/m2en/citation"
-        embed.description = "citationはメッセージリンクのプレビューを表示するDiscord Botです"
-        embed.color
-        embed.field("引用") {
-            "citationに閲覧権限が付与されているチャンネル内でメッセージリンクを送信すると、そのメッセージリンクのレビューをEmbedとして送信します。\n" +
-            "(スレッド内でも、プライベートで招待されていないなどの一連の条件を除き、自動で引用します)\n" +
-            "[詳しくはこちら](https://github.com/m2en/citation/blob/main/docs/function/quote.md)"
-        }
-        embed.field("引用スキップ") {
-            "リンクを `<>` で囲うと引用をスキップします"
-        }
-        embed.field("メッセージデバック") {
-            "メッセージを右クリックして出てくるメニュー(モバイル版はメッセージ長押し)からメッセージをコードブロックとして表示するデバック機能を利用できます。\n" +
-            "[詳しくはこちら](https://github.com/m2en/citation/blob/main/docs/function/debug.md)"
-        }
-        embed.field("リンク") {
-            "・[リポジトリ](https://github.com/m2en/citation)\n" +
-            "・[公式ドキュメント](https://github.com/m2en/citation/blob/main/docs/README.md)"
-        }
-        embed.footer{
-            text = "${interaction.user.username} - citation by m2en"
+        val embed = EmbedBuilder().apply {
+            title = "ヘルプ - v${getCitationVersion()}"
+            url = "https://github.com/m2en/citation"
+            description = "citationはメッセージリンクのプレビューを表示するDiscord Botです"
+            field {
+                name = "引用"
+                value = "citationに閲覧権限が付与されているチャンネル内でメッセージリンクを送信すると、そのメッセージリンクのレビューをEmbedとして送信します。\n" +
+                        "(スレッド内でも、プライベートで招待されていないなどの一連の条件を除き、自動で引用します)\n" +
+                        "[詳しくはこちら](https://github.com/m2en/citation/blob/main/docs/function/quote.md)"
+            }
+            field {
+                name = "引用スキップ"
+                value =  "リンクを `<>` で囲うと引用をスキップします"
+            }
+            field {
+                name = "メッセージデバック"
+                value = "メッセージを右クリックして出てくるメニュー(モバイル版はメッセージ長押し)からメッセージをコードブロックとして表示するデバック機能を利用できます。\n" +
+                        "[詳しくはこちら](https://github.com/m2en/citation/blob/main/docs/function/debug.md)"
+            }
+            field {
+                name = "リンク"
+                value = "・[リポジトリ](https://github.com/m2en/citation)\n" +
+                        "・[公式ドキュメント](https://github.com/m2en/citation/blob/main/docs/README.md)"
+            }
+            footer {
+                text = "${interaction.user.username} - citation by m2en"
+            }
         }
 
         responseBehavior.respond { embeds = MutableList(1) { embed } }
