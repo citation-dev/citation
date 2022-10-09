8821ef2df47786430fad016dd6e283a6d67a5965
Sakuma Sho <me@m2en.dev>
2022-10-10 01:07:07 +0900

    refactor: remove dotenv getter

--- src/main/kotlin/dev/m2en/citation/command/message/chat/RegisterCommand.kt
+++ src/main/kotlin/dev/m2en/citation/command/message/chat/RegisterCommand.kt
@@ -9,22 +9,23 @@ import dev.kord.core.entity.Message
 import dev.kord.core.entity.ReactionEmoji
 import dev.m2en.citation.handler.MessageHandler
 import dev.m2en.citation.utils.Logger
-import io.github.cdimascio.dotenv.dotenv
 
-object RegisterCommand: MessageHandler {
+class RegisterCommand(private val guildId: Snowflake) : MessageHandler {
     override suspend fun canProcess(message: Message): Boolean = message.author?.isBot == false
 
     override suspend fun messageHandle(message: Message) {
         // <メンション> register
-        if(message.content !== "<@${message.kord.selfId}> register") {
+        if (message.content !== "<@${message.kord.selfId}> register") {
             return
         }
 
-        val dotenv = dotenv()
         val member = message.author?.id?.let { message.getGuild().getMemberOrNull(it) } ?: return
 
-        if(Permission.isDangerPermission(member.getPermissions())) {
-            message.reply { content = "> **エラー:** 権限が足りません。このコマンドを実行するには **サーバーの管理権限(`ManageGuild`) または 管理者権限(`Administrator`)** が必要です。" }
+        if (Permission.isDangerPermission(member.getPermissions())) {
+            message.reply {
+                content =
+                    "> **エラー:** 権限が足りません。このコマンドを実行するには **サーバーの管理権限(`ManageGuild`) または 管理者権限(`Administrator`)** が必要です。"
+            }
             return
         }
 
@@ -35,25 +36,31 @@ object RegisterCommand: MessageHandler {
         try {
             Logger.sendInfo("Application Commandの登録を開始します....")
 
-            registerApplicationCommand(kord, dotenv.get("GUILD_ID"))
+            registerApplicationCommand(kord, guildId)
 
             Logger.sendInfo("Application Commandの登録に成功しました")
-            statusMessage.edit { content = "Application Commandの登録に成功しました\n(コマンドが反映していない場合はDiscordクライアントの再起動をしてください)" }
+            statusMessage.edit {
+                content =
+                    "Application Commandの登録に成功しました\n(コマンドが反映していない場合はDiscordクライアントの再起動をしてください)"
+            }
         } catch (e: Exception) {
             Logger.sendError("Application Commandの登録に失敗しました。", e)
-            statusMessage.edit { content = "Application Commandの登録に失敗しました。詳細はログを参照してください。\n登録方法が正しいかcitation docsを参照することもお勧めします。\n<https://citation.m2en.dev/getting-started/>" }
+            statusMessage.edit {
+                content =
+                    "Application Commandの登録に失敗しました。詳細はログを参照してください。\n登録方法が正しいかcitation docsを参照することもお勧めします。\n<https://citation.m2en.dev/getting-started/>"
+            }
         }
     }
 
-    private suspend fun registerApplicationCommand(kord: Kord, guildId: String) {
+    private suspend fun registerApplicationCommand(kord: Kord, guildId: Snowflake) {
         kord.createGuildChatInputCommand(
-            Snowflake(guildId),
+            guildId,
             "help",
             "ヘルプを表示します"
         )
 
         kord.createGuildMessageCommand(
-            Snowflake(guildId),
+            guildId,
             "Debug"
         )
     }
