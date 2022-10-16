c45b6f23351c3211e146f4627308760cdd9deabe
m2en <me@m2en.dev>
2022-10-16 15:24:26 +0900

    fix: github-query - code

--- src/main/kotlin/dev/m2en/citation/command/QueryCommand.kt
+++ src/main/kotlin/dev/m2en/citation/command/QueryCommand.kt
@@ -40,7 +40,7 @@ private fun searchDocs(interaction: SlashCommandInteraction, query: String) {
 
 private fun searchGitHub(interaction: SlashCommandInteraction, query: String) {
     when (query) {
-        "code" -> replyRecruit(interaction, "Code", "$githubLink/$query")
+        "code" -> replyRecruit(interaction, "Code", githubLink)
         "issues" -> replyRecruit(interaction, "Issues", "$githubLink/$query")
         "pulls" -> replyRecruit(interaction, "Pull requests", "$githubLink/$query")
         "discussions" -> replyRecruit(interaction, "Discussions", "$githubLink/$query")
